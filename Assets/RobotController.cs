e6f5ce9f5965b7ada931c8b59f65f16c5c87e9e8
HienaDev <ant.rodrigues1999@gmail.com>
2024-10-25 18:10:45 +0100

    Add input field for code

--- Assets/RobotController.cs
+++ Assets/RobotController.cs
@@ -2,17 +2,28 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using System;
+using TMPro;
+using System.Linq;
+using static UnityEditor.Progress;
 
 public class RobotController : MonoBehaviour
 {
-
+    // The queue of the orders, saving the method and its corresponding parameters
     private Queue<(Action<MethodParameters>, MethodParameters)> orders;
 
+    // Dictionary that isn't being used but might come in handy for intellisense
     private Dictionary<string, Action<MethodParameters>> methodsDictionary;
 
+    // Bool to keep in check if the coroutine is running so that we can start a new one
     private bool coroutineRunning = false;
 
     private Rigidbody2D rb;
+
+    // Text that the input field writes to
+    [SerializeField] private TextMeshProUGUI code;
+
+    // Save each line of code here split by '\n'
+    private string[] ordersCode;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,45 +31,143 @@ public class RobotController : MonoBehaviour
         orders = new Queue<(Action<MethodParameters>, MethodParameters)>();
         methodsDictionary = new Dictionary<string, Action<MethodParameters>>();
 
+        rb = GetComponent<Rigidbody2D>();
+
         methodsDictionary.Add("walk", Walk);
         methodsDictionary.Add("jump", Jump);
         methodsDictionary.Add("wait", Wait);
 
+        ordersCode = code.text.Split('\n');
 
-        rb = GetComponent<Rigidbody2D>();
+        for (int i = 0; i < ordersCode.Length; i++)
+        {
+            string[] splitTemp;
 
-        orders.Enqueue((Jump, new JumpParameters(40f)));
-        orders.Enqueue((Wait, new WaitParameters(2f)));
-        orders.Enqueue((Jump, new JumpParameters(40f)));
-        orders.Enqueue((Walk, new WalkParameters(50f, 4f)));
-        orders.Enqueue((Wait, new WaitParameters(2f)));
-        //orders.Enqueue(Walk(50f, 1f));
-        //orders.Enqueue(Jump(200f));
-        //orders.Enqueue(Wait(2f));
-        //orders.Enqueue(Walk(-50f, 1f));
-        //orders.Enqueue(Wait(2f));
-        //orders.Enqueue(Jump(200f));
+            // If we are in the last string, remove last character, as there seems to be
+            // a problem with the last char of the input field, not allowing it to be parsed
+            // into a float
+            if(i ==  ordersCode.Length - 1)
+            {
+                ordersCode[ordersCode.Length - 1] = 
+                                    ordersCode[ordersCode.Length - 1]
+                                    .Remove(ordersCode[ordersCode.Length - 1].Length - 1);
+            }
 
+            // Split all the code by the characters showed under: 
+            // '(' and ')' contain the parameters
+            // ';' is essentially just removed
+            // ',' splits the parameters
+            // ' ' empty spaces are pretty much useless and are mainly used for identation (hopefully)
+            splitTemp = ordersCode[i].Split('(', ')', ';', ',', ' ');
+            // Remove all empty strings
+            splitTemp = splitTemp.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            //Read each order after being split
+            ReadOrders(splitTemp);
+        }
+        
+
+    }
+
+    public string RemoveWhitespace(string input)
+    {
+        return new string(input.ToCharArray()
+            .Where(c => !Char.IsWhiteSpace(c))
+            .ToArray());
+    }
+
+    private void ReadOrders(string[] ordersSplit)
+    {
+        // Need to add Try, but first we'll assume they are all correct
+        //      TryParse added but need other checks before assuming its correct
+        //      but right now the code only runes if both parameters were parsed
+
+        for (int i = 0; i < ordersSplit.Length; i++)
+        {
+            // Remove all extra white spaces from the individual strings so " 1" becomes "1"
+            ordersSplit[i] = RemoveWhitespace(ordersSplit[i]);
+            Debug.Log($"{ordersSplit[i]}");
+
+            switch(ordersSplit[i].ToLower())
+            {
+                case "walk":
+                    //Try to parse the first parameter
+                    float parameter1;
+                    if (float.TryParse(ordersSplit[i + 1], out parameter1))
+                    {
+                        Debug.Log($"Parsed {ordersSplit[i + 1]}");
+                        //Try to parse the second parameter
+                        float parameter2;
+                        if (float.TryParse(ordersSplit[i + 2], out parameter2))
+                        {
+                            Debug.Log($"Parsed {ordersSplit[i + 2]}");
+                            orders.Enqueue((Walk, new WalkParameters(parameter1, parameter2)));
+                            Debug.Log("walk enqueued");
+                            i += 2;
+                        }
+                        else
+                        {
+                            Debug.Log($"Cant parse {ordersSplit[i + 2]}");
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log($"Cant parse {ordersSplit[i + 1]}");
+                    }
+
+
+
+
+                    break;
+
+                case "jump":
+                    //Try to parse the first parameter
+                    if (float.TryParse(ordersSplit[i + 1], out parameter1))
+                    {
+                        Debug.Log($"Parsed {ordersSplit[i + 1]}");
+                        orders.Enqueue((Jump, new JumpParameters(parameter1)));
+                        Debug.Log("Jump enqueued");
+                        i += 1;
+                    }
+                    else
+                    {
+                        Debug.Log($"Cant parse {ordersSplit[i + 1]}");
+                    }
+
+
+                    break;
+
+                case "wait":
+                    //Try to parse the first parameter
+                    if (float.TryParse(ordersSplit[i + 1], out parameter1))
+                    {
+                        Debug.Log($"Parsed {ordersSplit[i + 1]}");
+                        orders.Enqueue((Wait, new WaitParameters(parameter1)));
+                        Debug.Log("Wait enqueued");
+                        i += 1;
+                    }
+                    else
+                    {
+                        Debug.Log($"Cant parse {ordersSplit[i + 1]}");
+                    }
+
+
+                    break;
+
+                default:
+
+                    break;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(!coroutineRunning && orders.Count != 0)
-        //{
-        //    IEnumerator action = orders.Dequeue();
-        //    StartCoroutine(action);
-        //}
-
-        //if(Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    //StartCoroutine(Walk(50f, 5f));
-        //    //StartCoroutine(Jump(40));
-        //}
-
         ProcessOrders();
     }
 
+    //Method to process orders whenever no order is running
     private void ProcessOrders()
     {
         if (!coroutineRunning && orders.Count != 0)
@@ -68,6 +177,7 @@ public class RobotController : MonoBehaviour
         }
     }
 
+    // Assist method for the Walk Coroutine that takes some MethodParameters as input
     private void Walk(MethodParameters parameters)
     {
         if(parameters is WalkParameters)
@@ -78,6 +188,7 @@ public class RobotController : MonoBehaviour
             
     }
 
+    // Walking Coroutine
     private IEnumerator WalkCR(float speed, float duration)
     {
         coroutineRunning = true;
@@ -92,13 +203,14 @@ public class RobotController : MonoBehaviour
             yield return null;
         }
 
-        Debug.Log(time);
+        //Debug.Log(time);
 
         rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
 
         coroutineRunning = false;
     }
 
+    // Assist method for the Jump Coroutine that takes some MethodParameters as input
     private void Jump(MethodParameters parameters)
     {
         if (parameters is JumpParameters)
@@ -109,6 +221,7 @@ public class RobotController : MonoBehaviour
 
     }
 
+    // Jump Coroutine
     private IEnumerator JumpCR(float height)
     {
 
@@ -125,6 +238,7 @@ public class RobotController : MonoBehaviour
         coroutineRunning = false ;
     }
 
+    // Assist method for the Wait Coroutine that takes some MethodParameters as input
     private void Wait(MethodParameters parameters)
     {
         if (parameters is WaitParameters)
@@ -135,6 +249,7 @@ public class RobotController : MonoBehaviour
 
     }
 
+    // Wait Coroutine
     private IEnumerator WaitCR(float duration)
     {
 
